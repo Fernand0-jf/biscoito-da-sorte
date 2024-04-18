f0945032bb620060fa7d8d6a954409b4fb323b0e
Fernand0-jf <rubronm@hotmail.com>
2024-04-18 19:37:43 -0300

    alteracao dia 18/04/24

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import React,{useState} from 'react';
+import {View,Text,Button} from 'react-native';
 
-export default class App extends Component{
-    render(){
-       return(
-            <View>
-                <Text>Ola mundo</Text>
-            </View>
-        );
+export default function App(){
+    const [nome,setnome] = useState('jorge');
+    function entrar(){
+        alert('entrou');
     }
+
+    return(
+    <View style={{marginTop:25}}>
+        <Button title="Mudar Nome" onPress={entrar}/>
+        <Text style={{fontSize:19}}>{nome}</Text>
+    </View>
+    );
 }
+
