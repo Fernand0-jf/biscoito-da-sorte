c364b31e0259601a86b4d9911a7a1cdad010053f
Fernand0-jf <rubronm@hotmail.com>
2024-04-20 00:51:22 -0300

    19/04/24

--- App.js
+++ App.js
@@ -1,8 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {View,Text,StyleSheet,Image,TouchableOpacity} from 'react-native';
 
 export default function App(){
+    
+
     const [img,setImg] = useState(require('./src/biscoito.png'));
+    const [textoFrase,setTextoFrase] = useState('');
+
+    let frases = [
+        "Hoje é um bom dia para tentar algo novo!",
+        "Confie na sua intuição.",
+        "Uma oportunidade excitante surgirá em breve.",
+        "Sua habilidade de superar desafios é impressionante.",
+        "A sorte favorece o corajoso.",
+        "Mantenha o foco, mantenha o rumo.",
+        "Você encontrará o caminho se mantiver a mente aberta.",
+        "A persistência é o caminho do êxito.",
+        "Sua felicidade será encontrada ao seguir seu coração.",
+        "Aproveite as oportunidades que estão diante de você."
+    ];
+
+    function quebraBiscoito(){
+        let numeroAleatorio = Math.floor(Math.random() * frases.length)
+        setTextoFrase(' " ' +frases[numeroAleatorio]+ ' " ');
+        setImg(require('./src/biscoitoAberto.png'))
+    }
+
+    function reiniciarBiscoito(){
+        setTextoFrase('');
+        setImg(require('./src/biscoito.png'));
+    }
 
     return(
     <View style={styles.container}>
@@ -12,12 +39,20 @@ export default function App(){
         style={styles.img}
         />
 
-        <Text style={styles.textoFrase}>"Frase biscoito"</Text>
-        <TouchableOpacity style={styles.botao} onPress={()=>alert('')}>
+        <Text style={styles.textoFrase}>{textoFrase}</Text>
+
+        <TouchableOpacity style={styles.botao} onPress={quebraBiscoito}>
             <View style={styles.btnArea}>
                 <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
             </View>
         </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.botao,{marginTop:15, borderColor:'#121212'}]} onPress={reiniciarBiscoito}>
+            <View style={styles.btnArea}>
+                <Text style={[styles.btnTexto,{color:'#121212'}]}>Reiniciar Biscoito</Text>
+            </View>
+        </TouchableOpacity>
+
     </View>
     );
 }
@@ -44,14 +79,16 @@ const styles = StyleSheet.create({
         heigth:50,
         borderColor:'#dd7b22',
         borderWidth:2,
-        borderRadius:25,
+        borderRadius:25
     },
     btnArea:{
-        flex:1,
+        flex:0,
         justifyContent:'center',
         alignItems:'center'
     },
     btnTexto:{
-
+        fontSize:17,
+        fontWeight:'bold',
+        color:'#dd7b22'
     }
 });
